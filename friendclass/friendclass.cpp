918320327bcf6d518d15b2d0f992e914cffd0d30
zil-web <126854462+zil-web@users.noreply.github.com>
2023-05-10 13:21:11 +0700

    friend class2

--- friendclass/friendclass.cpp
+++ friendclass/friendclass.cpp
@@ -1,20 +1,42 @@
-// friendclass.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class siswa;
+
+class orang {
+private:
+	string nama;
+public:
+	void setnama(string pnama);
+	friend class siswa;
+};
+
+class siswa {
+private:
+	int id;
+public:
+	void setid(int pid);
+	void displayALL(orang& a);
+};
+
+void siswa::displayALL(orang& a) {
+	cout << id << endl << a.nama;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void siswa::setid(int pid) {
+	id = pid;
+}
+void orang::setnama(string pnama) {
+	nama = pnama;
+}
+int main()
+{
+	orang o;
+	o.setnama("joko kumat");
+	siswa s;
+	s.setid(1);
+	s.displayALL(o);
+	cout << endl;
+	system("pause");
+	return 0;
+}
